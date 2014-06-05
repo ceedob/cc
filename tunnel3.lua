7eed3eab1e9c0ddcb7bf2b9eea81dcb1abbf5a9b
Chris Dobson <chris@penguinleaf.com>
2014-06-04 20:51:42 -0400

    tunnel3.lua

---
+++ tunnel3.lua
@@ -0,0 +1,17 @@
+-- Tunnel3
+while true do
+	turtle.dig()
+	turtle.forward()
+	turtle.turnLeft()
+	turtle.dig()
+	turtle.forward()
+	turtle.digUp()
+	turtle.digDown()
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.dig()
+	turtle.forward()
+	turtle.digUp()
+	turtle.digDown()
+end
